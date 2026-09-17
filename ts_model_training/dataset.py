7e0221eb3b7469a1e777c614de876fceb6f9f7d2
M-Elboghdady <mahmoud.m.elboghdady@gmail.com>
2026-09-17 10:49:31 +0200

    Update dataset.py
    the elif was not aligned right even if it is right on my code!!!

--- ts_model_training/dataset.py
+++ ts_model_training/dataset.py
@@ -139,7 +139,7 @@ class TimeSeriesDataset:
         train_ind = self.splits['train']
         if model_type in ['gru', 'lstm', 'tcn', 'sand', 'mlp', 'mamba', 'mamba_fusion']:
             self.preproc = PreprocessorA(self)
-	elif model_type == 'ehrmamba_lab':
+	    elif model_type == 'ehrmamba_lab':
     	    self.preproc = PreprocessorEHRMamba(self)
         elif model_type in ['grud', 'interpnet']:
             self.preproc = PreprocessorB(self)
